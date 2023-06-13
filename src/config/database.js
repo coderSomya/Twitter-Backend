30a4e341341f57be85a1f3beeb9dcba6c0ab347d
Somya <isomya13@gmail.com>
2023-06-14 01:40:41 +0530

    added hashtag schema

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,9 @@
 import mongoose, { Mongoose } from "mongoose"
 import Tweet from "../models/tweet.js";
+import Hashtag from "../models/hashtag.js";
 
 export const connect = async()=>{
     await mongoose.connect('mongodb+srv://isomya13:%23iitAdv2512@cluster0.t49otyp.mongodb.net/');
 
-    Tweet.create({
-        content: "second tweet",
-        likes: 11,
-        noOfRetweets:3
-    })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {connect} from "./config/database.js"
+import Tweet from "./models/tweet.js";
+import Hashtag from "./models/hashtag.js";
 
 const app= express();
 
 app.listen(3000, async()=>{
     connect();
     console.log("app listening");
+
+    
 })
